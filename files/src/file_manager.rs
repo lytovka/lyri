5047a25fdd0c01685d8265337b0e872afae7782b
ivanlytovka <ivanlytovka@gmail.com>
2023-06-03 13:25:04 -0400

    resolve merge conflicts

--- files/src/file_manager.rs
+++ files/src/file_manager.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufReader, Write},
     str,
@@ -7,21 +8,23 @@ use std::{
 use genius::model::song::{ArtistSong, ArtistSongWithLyrics};
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug)]
+#[derive(Deserialize, Debug, Clone)]
 pub struct FileData {
     pub total: usize,
     pub songs: Vec<ArtistSong>,
 }
 
 impl FileData {
-    pub fn to_file_data_with_lyrics(&self, lyrics: Vec<String>) -> FileDataWithLyrics {
+    pub fn to_file_data_with_lyrics(&self, lyrics: HashMap<u32, String>) -> FileDataWithLyrics {
         FileDataWithLyrics {
             total: self.total,
             songs: self
                 .songs
                 .iter()
-                .zip(lyrics)
-                .map(|(song, lyrics)| song.to_artist_song_with_lyrics(lyrics))
+                .map(|song| match lyrics.get(&song.id) {
+                    Some(lyrics) => song.to_artist_song_with_lyrics(lyrics.to_owned()),
+                    None => song.to_artist_song_with_lyrics(String::from("")),
+                })
                 .collect(),
         }
     }
