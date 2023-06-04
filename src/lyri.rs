a4ce7949c6f9e6846d1031e1ef760e42488cecbf
ivanlytovka <ivanlytovka@gmail.com>
2023-06-04 00:20:35 -0400

    resolve

--- src/lyri.rs
+++ src/lyri.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, sync::Arc};
+
 use files::file_manager::{FileManager, SongsFileManager};
 use genius::{
     genius::Genius,
@@ -5,6 +7,7 @@ use genius::{
 };
 use processing::filters;
 use scraper::scraper::AppScraper;
+use tokio::sync::{mpsc, Mutex};
 
 fn find_arg_artist_from_hits(arg_artist: &str, genius_hits: Vec<Hit>) -> (u32, String) {
     let matched_hit = genius_hits
@@ -45,16 +48,38 @@ pub async fn lyri(args: Args) -> Result<(), Box<dyn std::error::Error>> {
 
     let res_file = SongsFileManager::read(file_path.as_str());
 
-    let scraper = AppScraper::new();
+    let res_copy = res_file.clone();
 
-    let mut lyric_vec: Vec<String> = vec![];
+    let (sender, mut receiver) = mpsc::channel::<(u32, String)>(res_file.songs.len());
+    let sender = Arc::new(Mutex::new(sender));
 
-    for song in &res_file.songs {
-        let lyrics = scraper.from_url(&song.url).await?;
-        lyric_vec.push(lyrics);
+    for song in res_file.songs {
+        let sender = Arc::clone(&sender);
+        tokio::spawn(async move {
+            let scraper = AppScraper::new();
+            match scraper.from_url(&song.url).await {
+                Ok(lyrics) => {
+                    let sender = sender.lock().await;
+                    match sender.send((song.id, lyrics)).await {
+                        Ok(res) => res,
+                        Err(err) => panic!("{}", err.to_string()),
+                    }
+                }
+                Err(err) => println!("Error: {}", err),
+            }
+        });
     }
 
-    let file_data_with_lyrics = res_file.to_file_data_with_lyrics(lyric_vec);
+    let mut lyrics_map: HashMap<u32, String> = HashMap::new();
+
+    while let Some((song_id, lyrics)) = receiver.recv().await {
+        lyrics_map.insert(song_id, lyrics);
+        if lyrics_map.len() == res_copy.songs.len() {
+            break;
+        }
+    }
+
+    let file_data_with_lyrics = res_copy.to_file_data_with_lyrics(lyrics_map);
 
     let file_json = json!({
         "total": file_data_with_lyrics.songs.len(),
