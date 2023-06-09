d58768fe8e0e1c121535bb6e0e43a9b24b67492c
ivanlytovka <ivanlytovka@gmail.com>
2023-06-08 22:42:22 -0400

    feat: add logging

--- genius/src/genius.rs
+++ genius/src/genius.rs
@@ -1,3 +1,5 @@
+use log::{error, info};
+
 use {
     crate::model::{
         artist::Artist,
@@ -9,25 +11,30 @@ use {
     serde::Deserialize,
 };
 
+const BASE_URL: &str = "https://api.genius.com";
 const PER_PAGE: u16 = 50;
+const GENIUS_ACCESS_TOKEN_ENV_VAR: &str = "GENIUS_ACCESS_TOKEN";
 
 #[derive(Deserialize, Debug)]
 struct Response<T> {
     response: T,
 }
 
-pub struct Genius<'a> {
+pub struct Genius {
     reqwest: Client,
     auth_token: String,
-    base_url: &'a str,
 }
 
-impl Genius<'_> {
+impl Genius {
     pub fn new() -> Self {
         Self {
-            auth_token: dotenv::var("GENIUS_ACCESS_TOKEN")
-                .expect("Could not find .env var OR the value is wrong"),
-            base_url: "https://api.genius.com",
+            auth_token: dotenv::var(GENIUS_ACCESS_TOKEN_ENV_VAR).expect(
+                format!(
+                    "Could not find environment variable `{}`. Make sure it's set in the `.env` file.",
+                    GENIUS_ACCESS_TOKEN_ENV_VAR
+                )
+                .as_str(),
+            ),
             reqwest: Client::new(),
         }
     }
@@ -36,7 +43,7 @@ impl Genius<'_> {
     pub async fn search(&self, q: &str) -> Result<Vec<Hit>, Error> {
         let request = self
             .reqwest
-            .get(format!("{}/search", self.base_url))
+            .get(format!("{}/search", BASE_URL))
             .query(&[("q", q)])
             .bearer_auth(&self.auth_token)
             .send()
@@ -47,17 +54,17 @@ impl Genius<'_> {
                 Ok(res_ok) => match res_ok.json::<Response<SearchResponse>>().await {
                     Ok(search_res) => Ok(search_res.response.hits.unwrap()),
                     Err(err) => {
-                        println!("Error while deserializing to SearchResponse: {:#?}", err);
+                        error!("Error while deserializing to SearchResponse: {:#?}", err);
                         Err(err)
                     }
                 },
                 Err(res_err) => {
-                    println!("Bad status code: {:?}", res_err.status());
+                    error!("Bad status code: {:?}", res_err.status());
                     Err(res_err)
                 }
             },
             Err(err) => {
-                println!("Unexpected result: {}", err);
+                error!("Unexpected result: {}", err);
                 Err(err)
             }
         }
@@ -67,7 +74,7 @@ impl Genius<'_> {
     pub async fn songs(&self, id: u32) -> Result<ArtistSong, Error> {
         let response = self
             .reqwest
-            .get(format!("https://api.genius.com/songs/{}", id))
+            .get(format!("{}/songs/{}", BASE_URL, id))
             .bearer_auth(&self.auth_token)
             .send()
             .await;
@@ -77,16 +84,16 @@ impl Genius<'_> {
                 Ok(res_ok) => match res_ok.json::<Response<SongResponse>>().await {
                     Ok(song_res) => Ok(song_res.response.song.unwrap()),
                     Err(err) => {
-                        println!("Error while deserializing to SongResponse: {:#?}", err);
+                        error!("Error while deserializing to SongResponse: {:#?}", err);
                         Err(err)
                     }
                 },
-                Err(err) => {
-                    println!("Bad status code: {:?}", err.status());
-                    Err(err)
-                }
+                Err(err) => Err(err),
             },
-            Err(e) => panic!("Unexpected result:\n{:#?}", e),
+            Err(err) => {
+                error!("Bad status code: {:?}", err.status());
+                Err(err)
+            }
         }
     }
 
@@ -94,7 +101,7 @@ impl Genius<'_> {
     pub async fn artists(&self, id: u32) -> Result<Artist, Error> {
         let response = self
             .reqwest
-            .get(format!("https://api.genius.com/artists/{}", id))
+            .get(format!("{}/artists/{}", BASE_URL, id))
             .bearer_auth(&self.auth_token)
             .send()
             .await;
@@ -104,16 +111,19 @@ impl Genius<'_> {
                 Ok(res_ok) => match res_ok.json::<Response<ArtistResponse>>().await {
                     Ok(artist_res) => Ok(artist_res.response.artist.unwrap()),
                     Err(err) => {
-                        println!("Error while deserializing to SongResponse: {:#?}", err);
+                        error!("Error while deserializing to SongResponse: {:#?}", err);
                         Err(err)
                     }
                 },
                 Err(err) => {
-                    println!("Bad status code: {:?}", err.status());
+                    error!("Bad status code: {:?}", err.status());
                     Err(err)
                 }
             },
-            Err(e) => panic!("Unexpected result:\n{:#?}", e),
+            Err(err) => {
+                error!("Bad status code: {:?}", err.status());
+                Err(err)
+            }
         }
     }
 
@@ -121,54 +131,48 @@ impl Genius<'_> {
     pub async fn artists_songs(&self, artist_id: u32) -> Result<Vec<ArtistSong>, Error> {
         let mut page: u16 = 1;
         let mut total_count: usize = 0;
-        let mut resulting_vector: Vec<ArtistSong> = vec![];
+        let mut songs_res: Vec<ArtistSong> = vec![];
         let spinner = cli::progress::fetch_progress_bar();
 
         loop {
             let response = self
                 .reqwest
-                .get(format!(
-                    "https://api.genius.com/artists/{}/songs",
-                    artist_id
-                ))
+                .get(format!("{}/artists/{}/songs", BASE_URL, artist_id))
                 .query(&[("page", page), ("per_page", PER_PAGE)])
                 .bearer_auth(&self.auth_token)
                 .send()
                 .await?;
 
-            match response.status() {
-                reqwest::StatusCode::OK => {
-                    match response.json::<Response<ArtistSongsResponse>>().await {
-                        Ok(res) => match res.response.songs {
-                            Some(songs) => {
-                                if songs.is_empty() {
-                                    spinner.finish_and_clear();
-                                    break Ok(resulting_vector);
-                                }
-                                total_count += songs.len();
-                                resulting_vector.extend(songs);
-                                page += 1;
+            match response.error_for_status() {
+                Ok(res_ok) => match res_ok.json::<Response<ArtistSongsResponse>>().await {
+                    Ok(res_parsed) => match res_parsed.response.songs {
+                        Some(songs) => {
+                            if songs.is_empty() {
+                                spinner.finish_and_clear();
+                                break Ok(songs_res);
                             }
-                            None => {
-                                if !resulting_vector.is_empty() {
-                                    println!("Returning {} songs", total_count);
-                                    spinner.finish_and_clear();
-                                    break Ok(resulting_vector);
-                                } else {
-                                    panic!("No song has been returned");
-                                }
+                            total_count += songs.len();
+                            songs_res.extend(songs);
+                            page += 1;
+                        }
+                        None => {
+                            if !songs_res.is_empty() {
+                                spinner.finish_and_clear();
+                                break Ok(songs_res);
+                            } else {
+                                error!("No songs have been returned");
                             }
-                        },
-                        Err(e) => panic!("Unexpected result:\n{:#?}", e),
-                    }
-                }
-                bad_status_code => {
-                    if !resulting_vector.is_empty() {
-                        println!("Returning {} songs", total_count);
+                        }
+                    },
+                    Err(e) => error!("Error while deserializing to ArtistSongsResponse: {:#?}", e),
+                },
+                Err(res_bad) => {
+                    if !songs_res.is_empty() {
+                        info!("Returning {} songs", total_count);
                         spinner.finish_and_clear();
-                        break Ok(resulting_vector);
+                        break Ok(songs_res);
                     } else {
-                        panic!("Bad status code {:?}", bad_status_code);
+                        error!("Bad status code {:?}", res_bad.status());
                     }
                 }
             }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ mod args;
 mod lyri;
 
 use clap::Parser;
+use env_logger::Env;
 use {crate::lyri::lyri, args::Args, tokio};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let env = Env::default().default_filter_or("info");
+    env_logger::init_from_env(env);
     let args = Args::parse();
     lyri(args).await?;
     Ok(())
