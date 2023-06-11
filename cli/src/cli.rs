0e09277f047f894301c7914b95c7194c2abb5a19
ivanlytovka <ivanlytovka@gmail.com>
2023-06-11 19:49:18 -0400

    add cli options and comments

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -9,12 +9,25 @@ pub struct Cli {
 
 #[derive(Subcommand)]
 pub enum Commands {
-    /// Search for an artist.
+    /// Retrieves lyrics for a specific artist.
     Artist(ArtistArgs),
 }
 
 #[derive(Args)]
 pub struct ArtistArgs {
+    /// Name of the artist
     #[arg(short, long)]
     pub name: String,
+
+    /// Number of songs to retrieve. If not specified, all songs will be retrieved
+    #[arg(short, long)]
+    pub limit: Option<u32>,
+
+    /// Filter songs by anti-pattern for title
+    #[arg(short, long)]
+    pub antipattern: Option<String>,
+
+    /// Include features in the results. If not specified, features will be excluded
+    #[arg(short, long)]
+    pub features: Option<bool>,
 }
