3944441531060ba9d743251fb3416287c8db68fd
ivanlytovka <ivanlytovka@gmail.com>
2023-06-11 22:46:35 -0400

    fix comment

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -31,7 +31,7 @@ pub struct ArtistArgs {
     #[arg(short, long)]
     pub features: Option<bool>,
 
-    /// Sort songs ("popularity" or "title"). If not specified, songs will be sorted by alphabetical order ("title")
+    /// Sort returned songs. If not specified, songs will be sorted by alphabetical order ("title"). Acceptable values: "popularity" or "title"
     #[arg(short, long)]
     pub sort: Option<String>,
 }
